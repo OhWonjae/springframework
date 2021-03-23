64c96e8dbb2cf73f6cebb0fb7c9842595564b3c8
COM <COM@DESKTOP-L2F7LSG>
2021-03-23 09:00:37 +0900

    commit

--- src/main/java/com/mycompany/webapp/dto/Board.java
+++ src/main/java/com/mycompany/webapp/dto/Board.java
@@ -1,7 +1,6 @@
 package com.mycompany.webapp.dto;
 
 public class Board {
-<<<<<<< HEAD
 	public int bno;
 	public String btitle;
 	public String bcontent;
@@ -33,35 +32,4 @@ public class Board {
 
 	
 }
-=======
-	private int bno;
-	private String btitle;
-	private String bcontent;
-	private String bwriter;
-	
-	public int getBno() {
-		return bno;
-	}
-	public void setBno(int bno) {
-		this.bno = bno;
-	}
-	public String getBtitle() {
-		return btitle;
-	}
-	public void setBtitle(String btitle) {
-		this.btitle = btitle;
-	}
-	public String getBcontent() {
-		return bcontent;
-	}
-	public void setBcontent(String bcontent) {
-		this.bcontent = bcontent;
-	}
-	public String getBwriter() {
-		return bwriter;
-	}
-	public void setBwriter(String bwriter) {
-		this.bwriter = bwriter;
-	}
-}
->>>>>>> branch 'master' of https://github.com/OhWonjae/springframework.git
+
